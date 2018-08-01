a10f6c310cd197fa6ef0d62cec82a0a7fd759cdd
sun <94412108@stud.sju.edu.tw>
2018-08-01 17:10:08 +0800

    remove image and other file move test thead and test data

--- app/index.js
+++ app/index.js
@@ -5,143 +5,15 @@ import { BrowserRouter }                   from "react-router-dom";
 //Components
 import List                                from './components/list';
 
+//Jsons
+import thead                               from './public/json/thead.json';
+import data                                from './public/json/data.json';
+
 //CSS
 import './public/stylesheets/style.scss';
 
-const columns = [
-  {
-      "title"     : "鱼种",
-      "columnKey" : "name",
-      "total"     : "共计",
-      "link"      : true,
-      "path"      : "info",
-      "sort"      : true
-  },
-  {
-      "title"     : "炮倍",
-      "columnKey" : "rate",
-      "sort"      : true
-  },
-  {
-      "title"     : "单价",
-      "columnKey" : "fishMoney"
-  },
-  {
-      "title"     : "总价值",
-      "columnKey" : "totalMoney",
-      "total"     : ""
-  },
-  {
-      "title"     : "子弹收入",
-      "columnKey" : "bulletMoney"
-  },
-  {
-      "title"     : "盈利",
-      "columnKey" : "earn",
-      "total"     : ""
-  },
-  {
-      "title"     : "仍存活(条)",
-      "columnKey" : "alive",
-      "total"     : ""
-  },
-  {
-      "title"     : "被捕获",
-      "columnKey" : "dead"
-  },
-  {
-    "title"       : "動作",
-    "columnKey"   : "action",
-    "action"      : [
-      {
-        "key"   : "edit",
-        "text"  : "修改",
-        "icon"  : ""
-      },
-      {
-        "key"   : "delete",
-        "text"  : "修改",
-        "icon"  : ""
-      },
-      {
-        "key"   : "info",
-        "text"  : "修改",
-        "icon"  : ""
-      }
-    ]
-  }
-]
-
 const total   = 10;
 const limit   = 10;
-const data    = [
-  {
-    "id"             : "1",
-    "name"           : "名稱1",
-    "rate"           : 100,
-    "fishMoney"      : 1000,
-    "totalMoney"     : 0.8,
-    "bulletMoney"    : 0.7,
-    "earn"           : 10,
-    "alive"          : 120,
-    "dead"           : ""
-  },
-  {
-    "id"             : "2",
-    "name"           : "名稱2",
-    "rate"           : 100,
-    "fishMoney"      : 1000,
-    "totalMoney"     : 0.8,
-    "bulletMoney"    : 0.7,
-    "earn"           : 10,
-    "alive"          : 120,
-    "dead"           : ""
-  },
-  {
-    "id"             : "3",
-    "name"           : "名稱3",
-    "rate"           : 100,
-    "fishMoney"      : 1000,
-    "totalMoney"     : 0.4,
-    "bulletMoney"    : 0.7,
-    "earn"           : 10,
-    "alive"          : 120,
-    "dead"           : ""
-  },
-  {
-      "id"             : "4",
-    "name"           : "名稱4",
-    "rate"           : 100,
-    "fishMoney"      : 1000,
-    "totalMoney"     : 0.8,
-    "bulletMoney"    : 0.7,
-    "earn"           : 10,
-    "alive"          : 120,
-    "dead"           : ""
-  },
-  {
-      "id"             : "5",
-    "name"           : "名稱5",
-    "rate"           : 100,
-    "fishMoney"      : 1000,
-    "totalMoney"     : 0.8,
-    "bulletMoney"    : 0.7,
-    "earn"           : 10,
-    "alive"          : 120,
-    "dead"           : ""
-  },
-  {
-      "id"             : "6",
-    "name"           : "名稱6",
-    "rate"           : 100,
-    "fishMoney"      : 1000,
-    "totalMoney"     : 0.8,
-    "bulletMoney"    : 0.7,
-    "earn"           : 10,
-    "alive"          : 120,
-    "dead"           : ""
-  }
-]
 
 export default class Index extends React.Component{
   constructor(props){
@@ -161,7 +33,7 @@ export default class Index extends React.Component{
         <List 
             total           = { total }
             limit           = { limit }
-            columns         = { columns }
+            columns         = { thead['test'] }
             data            = { data }
             currentPage     = "1"
             paginationStyle = "model1"
