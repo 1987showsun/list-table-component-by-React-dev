f4b28f3b5ac83e6937bc7ecf61f68e5926327289
sun <94412108@stud.sju.edu.tw>
2018-08-06 11:56:01 +0800

    no message

--- app/components/pagination/index.js
+++ app/components/pagination/index.js
@@ -3,7 +3,7 @@ import { Link }                     from 'react-router-dom';
 import { FaAngleLeft,FaAngleRight } from 'react-icons/fa';
 
 //Components
-import Item           from './item';
+import Item                         from './item';
 
 import './pagination.scss';
 
@@ -13,11 +13,12 @@ export default class Pagination extends React.Component{
         super(props);
         this.state = {
             paginationStyle : props.paginationStyle || "model1",
-            match           : props.match || {},
-            path            : props.path  || "",
-            serach          : props.serach|| "",
-            total           : props.total || 0,
-            limit           : props.limit || 0,
+            match           : props.match           || {},
+            currentPage     : props.currentPage     || 1,
+            path            : props.path            || "",
+            serach          : props.serach          || "",
+            total           : props.total           || 0,
+            limit           : props.limit           || 10,
             pagination      : [],
         }
     }
@@ -29,21 +30,22 @@ export default class Pagination extends React.Component{
     componentWillReceiveProps(nextProps) {
         this.setState({
             paginationStyle : nextProps.paginationStyle || "model1",
-            total           : nextProps.total,
-            limit           : nextProps.limit,
-            match           : nextProps.match || {},
-            path            : nextProps.path  || "",
-            serach          : nextProps.serach|| "",
+            total           : nextProps.total           || 0,
+            limit           : nextProps.limit           || 10,
+            match           : nextProps.match           || {},
+            currentPage     : nextProps.currentPage     || 1,
+            path            : nextProps.path            || "",
+            serach          : nextProps.serach          || "",
         },()=>{
             this.paginationPush();
         })
     }
 
     paginationPush(){
-        const pagination = [];
-        const total      = this.state.total;
-        const limit      = this.state.limit;
-        const totalPage = Math.ceil(total/limit);
+        const pagination  = [];
+        const total       = this.state.total;
+        const limit       = this.state.limit;
+        const totalPage   = Math.ceil(total/limit);
 
         switch( this.state.paginationStyle ){
             case "model1":
@@ -53,8 +55,7 @@ export default class Pagination extends React.Component{
                 break;
             
             case "model2":
-                let match   = this.state.match;
-                let current = match['params']['current']!=undefined? Number(match['params']['current']) : 1;
+                let current   = Number(this.state.currentPage);
                 if( current==1 ){
                     pagination[0]= <li key="1" className="pointerEvents"><Link to={`${this.state.path}/${current-1}${this.state.serach}`} onClick={this.callback.bind(this,current-1)} className="changePageBtn"><FaAngleLeft/></Link></li>;
                     pagination[1]= <li key="2" className="currentInfo">{`${current} / ${totalPage}`}</li>
